86b95a5db3474e63e6d9ea0b410e0ae0cde4bc98
Fezz <jonnicwolf@gmail.com>
2024-02-29 15:11:45 -0500

    Initial Commit

---
+++ swift_click_app/swift_click_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_click_appApp.swift
+//  swift_click_app
+//
+//  Created by Jonathan Narine on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_click_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
