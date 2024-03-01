27676d5a2b204a7806fac6df0d4184dc866ac03d
Fezz <jonnicwolf@gmail.com>
2024-02-29 20:41:02 -0500

    adds buy increment and bug fixes

--- swift_click_app/ContentView.swift
+++ swift_click_app/ContentView.swift
@@ -3,13 +3,29 @@ import SwiftUI
 struct ContentView: View {
     
     @State var currentNumber: Int = 0
+    @State var increaseIncrememnt: Bool = false
+    @State var incrementNumber: Int = 1
     
     func increment() {
-        currentNumber+=1
+        currentNumber+=incrementNumber
+        
+        if currentNumber >= 3 {
+            increaseIncrememnt = true
+        }
+        else {increaseIncrememnt = false}
     }
     
     func reset() {
         currentNumber = 0
+        increaseIncrememnt = false
+        incrementNumber = 1
+    }
+    
+    func buyIncrement() {
+        if currentNumber >= 3 {
+            currentNumber -=  3
+            incrementNumber += 1
+        }
     }
         
     var body: some View {
@@ -19,7 +35,7 @@ struct ContentView: View {
             Button(action: {
                 increment()
             }) {
-                Text("Increment")
+                Text("Increment +\(incrementNumber)")
                     .padding()
             }
             .background(Color.blue)
@@ -35,6 +51,18 @@ struct ContentView: View {
             .background(Color.blue)
             .foregroundColor(.white)
             .cornerRadius(10)
+            
+            if increaseIncrememnt {
+                Button(action: {
+                    buyIncrement()
+                }) {
+                    Text("Upgrade increment for 3 pts")
+                        .padding()
+                }
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
         }
         .frame(minWidth: 0, maxWidth: .infinity)
         .controlSize(.large)
